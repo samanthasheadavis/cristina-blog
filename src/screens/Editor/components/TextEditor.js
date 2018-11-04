635330b6ab1f4cd1fdac807a9543a960b11c8773
Samantha Davis <samantha.shea.davis@gmail.com>
2018-11-04 11:38:47 -0500

    uploadImage function

--- src/screens/Editor/components/TextEditor.js
+++ src/screens/Editor/components/TextEditor.js
@@ -7,27 +7,35 @@ import { wstyles } from "../../../../node_modules/react-draft-wysiwyg/dist/react
 class TextEditor extends React.Component {
   // Converts body text (if present) from raw DraftJS object into formatted text
   // Else creates new empty draft js object
-  constructor(props) {
-    super(props);
-    if (this.props.self.state.body) {
-      this.state = {
-        editorState: EditorState.createWithContent(
+
+  state = {
+    uploadImages: [],
+    editorState: this.props.self.state.body
+      ? EditorState.createWithContent(
           convertFromRaw(this.props.self.state.body)
         )
-      };
-    } else {
-      this.state = { editorState: EditorState.createEmpty() };
-    }
-    // saving raw body to editor state
-    // manually validating editor body
-    this.onChange = editorState => {
-      this.setState({ editorState });
-      const body = convertToRaw(this.state.editorState.getCurrentContent());
-      this.props.self.setState(
-        { body: body },
-        this.props.self.validateField("body", body)
-      );
-    };
+      : EditorState.createEmpty()
+  };
+
+  // saving raw body to editor state
+  // manually validating editor body
+  onChange = editorState => {
+    this.setState({ editorState });
+    const body = convertToRaw(this.state.editorState.getCurrentContent());
+    this.props.self.setState(
+      { body: body },
+      this.props.self.validateField("body", body)
+    );
+  };
+
+  // Function to upload images locally to preview in editor
+  _uploadImage(file) {
+    console.log(this.state.uploadImages);
+    const imageObject = { file: file, localSrc: URL.createObjectURL(file) };
+    this.setState([...this.state.uploadImages, imageObject]);
+    return new Promise((resolve, reject) => {
+      resolve({ data: { link: imageObject.localSrc } });
+    });
   }
 
   render() {
@@ -38,6 +46,9 @@ class TextEditor extends React.Component {
         <Paper elevation={2} style={{ padding: 10, flex: 1, marginTop: 5 }}>
           <Editor
             editorState={this.state.editorState}
+            toolbar={{
+              image: { uploadCallback: this._uploadImage.bind(this) }
+            }}
             wrapperClassName="wrapper-class"
             editorClassName="editor-class"
             toolbarClassName="toolbar-class"
